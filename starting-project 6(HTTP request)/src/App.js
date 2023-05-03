805ef053c0f6b18ed744460b0ea2f5a6d6718a7a
parag-mer <paragmer2001@gmail.com>
2023-05-03 10:28:13 +0530

    changes

---
+++ starting-project 6(HTTP request)/src/App.js
@@ -0,0 +1,103 @@
+import React , {useState , useEffect} from 'react';
+
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
+
+function App() {
+  const [movies , setMovies] = useState([]);
+  const [isLoading , setIsLoading] = useState(false);
+  const [error , setError] = useState(null);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  },[]);
+
+  async function fetchMoviesHandler()
+  {
+    setIsLoading(true);
+    setError(null);
+    try
+    {
+      const response = await fetch("https://rx-udemy-default-rtdb.firebaseio.com/movies.json");
+      
+
+      if(!response.ok)
+      {
+         throw new Error("something went wrong");
+      }
+        const data = await response.json();
+
+      const loadedMovies = [];
+      for(const key in data)
+      {
+        loadedMovies.push({
+          id : key,
+          title : data[key].title,
+          openingText : data[key].openingText,
+          releaseDate : data[key].releaseDate
+        })
+      }
+      // const transformedMovies = data.results.map(movieData => { 
+      //   return{
+      //     id : movieData.episide_id,
+      //     title : movieData.title,
+      //     openingText : movieData.opening_crawl,
+      //     releaseDate : movieData.release_date
+      //   }
+      // })
+      setMovies(loadedMovies);
+      setIsLoading(false);
+    }
+    catch(error)
+    {
+      setError(error.message);
+      setIsLoading(false);
+    }
+    
+  }
+
+  let content = <p>No movies found</p>;
+
+  if(movies.length > 0)
+  {
+    content = <MoviesList movies = {movies}/>;
+  }
+  if(error)
+  {
+    content = <p>{error}</p>;
+  }
+  if(isLoading)
+  {
+    content = <p>Please wait data is loading</p>;
+  }
+
+  async function addMovieHandler(movie)
+  {
+    const response = await fetch("https://rx-udemy-default-rtdb.firebaseio.com/movies.json" , {
+      method :"POST",
+      body : JSON.stringify(movie),
+      headers : {
+         'Content-type' : 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie = {addMovieHandler}/>
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {content}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
