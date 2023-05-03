c45f65d563dec810483b89f24cec0dc35b42da3c
parag-mer <paragmer2001@gmail.com>
2023-05-03 10:53:47 +0530

    code

--- starting-project 7(forms & validation)/src/components/SimpleInput.js
+++ starting-project 7(forms & validation)/src/components/SimpleInput.js
@@ -1,9 +1,21 @@
+import {useState} from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName , setEnteredName] = useState();
+  const enteredNameHandler = event => {
+    setEnteredName(event.target.value);
+  }
+
+  const formHandler = event => {
+    event.preventDefault();
+    console.log(enteredName);
+  }
+
   return (
-    <form>
+    <form onSubmit={formHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={enteredNameHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
