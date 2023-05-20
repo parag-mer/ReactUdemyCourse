e91bd64ab3b2eabdeb35faf4bdd0bfabb44a6c83
parag-mer <paragmer2001@gmail.com>
2023-05-20 18:51:22 +0530

    new code

---
+++ 06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -0,0 +1,8 @@
+
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails()
+{
+        return(
+        <MeetupDetail image='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png' title ='first meetup' address='new address' description='new description'/>);
+}
